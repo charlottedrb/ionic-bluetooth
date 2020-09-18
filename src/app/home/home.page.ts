12d0372b83d1b4fbdab7b82a9fd179dbb61a4e5e
charlottedrb <c.derbaghdassarian@gmail.com>
2020-09-18 15:29:15 +0200

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BLE } from '@ionic-native/ble/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+
+}
