ce00f6a6c3b4f6a04d7df15eb018458bf49865ab
charlottedrb <c.derbaghdassarian@gmail.com>
2020-09-18 15:50:08 +0200

    test avec angular et cordova

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { BLE } from '@ionic-native/ble/ngx';
 
 @Component({
@@ -7,7 +7,24 @@ import { BLE } from '@ionic-native/ble/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  devices: any[] = [];
 
-  constructor() {}
+  constructor(private ble: BLE, private ngZone: NgZone) {
 
+  }
+
+  scan() {
+    this.devices = [];
+    this.ble.scan([], 15).subscribe(
+      device => this.onDeviceDiscovered(device)
+    );
+  }
+
+  onDeviceDiscovered(device) {
+    console.log('Discovered' + JSON.stringify(device, null, 2));
+    this.ngZone.run(() => {
+      this.devices.push(device)
+      console.log(device)
+    })
+  }
 }
